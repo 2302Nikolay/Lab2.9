1506bda563eac936af5237415a1e2411b395b115
2302Nikolay <Malzewn@gmail.com>
2022-10-04 08:25:00 +0300

    ?

---
+++ Проработка примеров/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+import sys
+
+
+class TailRecurseException:
+    def __init__(self, args, kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+if __name__ == '__main__':
+    print()
